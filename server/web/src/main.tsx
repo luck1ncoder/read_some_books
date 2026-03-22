fbbff4bc630401a017f7eee8e16bea3282e46ab8
zhangxinming1 <zhangxinming1@xiaohongshu.com>
2026-03-22 18:25:05 +0800

    chore: scaffold extension and server project structure

---
+++ server/web/src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>Web Highlighter Loading...</div>
+  </StrictMode>
+)
